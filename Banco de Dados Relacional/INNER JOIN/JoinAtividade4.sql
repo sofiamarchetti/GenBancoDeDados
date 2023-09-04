f1f8f6d64207efc606f2b07a21ed2057712e7e67
Sofia Marchetti <113708530+sofiamarchetti@users.noreply.github.com>
2023-09-04 14:53:26 -0300

    Update JoinAtividade4.sql

--- Banco de Dados Relacional/INNER JOIN/JoinAtividade4.sql
+++ Banco de Dados Relacional/INNER JOIN/JoinAtividade4.sql
@@ -1,5 +1,4 @@
 create database db_cidade_dos_vegetais;
-
 use db_cidade_dos_vegetais;
 
 create table tb_categorias(
@@ -12,7 +11,7 @@ create table tb_categorias(
 create table tb_produtos(
     id bigint auto_increment,
     nome varchar(255) not null,
-    distribuidora varchar(255) not null,
+    fazenda varchar(255) not null,
     preco decimal not null,
     quantidade_estoque INT(3),
     id_categorias BIGINT,
@@ -21,29 +20,29 @@ create table tb_produtos(
 
 ALTER TABLE tb_produtos ADD CONSTRAINT fk_id_categorias FOREIGN KEY (id_categorias) REFERENCES tb_categorias (id);
 
-insert into tb_categorias (nome,categoria) values ("Antigripais","Medicamentos");
-insert into tb_categorias (nome,categoria) values ("Anticoncepcionais","Medicamentos");
-insert into tb_categorias (nome,categoria) values ("Produtos para o cabelo","Cosméticos");
-insert into tb_categorias (nome,categoria) values ("Maquiagens","Cosméticos");
-insert into tb_categorias (nome,categoria) values ("Proteção pessoal","Higiene");
+insert into tb_categorias (nome,categoria) values ("Frutas refrigeradas","Frutas");
+insert into tb_categorias (nome,categoria) values ("Legumes refrigerados","Legumes");
+insert into tb_categorias (nome,categoria) values ("Verduras refrigeradas","Verduras");
+insert into tb_categorias (nome,categoria) values ("Sucos","Industrializados");
+insert into tb_categorias (nome,categoria) values ("Frutas sem refrigeração","Frutas");
 
-insert into tb_produtos (nome,fabricante,preco,quantidade_estoque,id_categorias) values ("Desentupex","Quemanto",38.12,10,1);
-insert into tb_produtos (nome,fabricante,preco,quantidade_estoque,id_categorias) values ("Progestina","Queprerv",14.33,24,2);
-insert into tb_produtos (nome,fabricante,preco,quantidade_estoque,id_categorias) values ("Escova de cabelo","Chococon",19.54,17,3);
-insert into tb_produtos (nome,fabricante,preco,quantidade_estoque,id_categorias) values ("Batom","Tuquetem",43.60,74,4);
-insert into tb_produtos (nome,fabricante,preco,quantidade_estoque,id_categorias) values ("Máscara tripla","Calabre",36.91,23,5);
-insert into tb_produtos (nome,fabricante,preco,quantidade_estoque,id_categorias) values ("Dipirona","Vranca",19.95,58,1);
-insert into tb_produtos (nome,fabricante,preco,quantidade_estoque,id_categorias) values ("Gel de cabelo","Tuquetem",45.70,12,3);
-insert into tb_produtos (nome,fabricante,preco,quantidade_estoque,id_categorias) values ("Blush","Coco",58.90,3,4);
+insert into tb_produtos (nome,fazenda,preco,quantidade_estoque,id_categorias) values ("Cenoura","Caranguejo",31.24,4,2);
+insert into tb_produtos (nome,fazenda,preco,quantidade_estoque,id_categorias) values ("Lima","Costura",95.35,44,5);
+insert into tb_produtos (nome,fazenda,preco,quantidade_estoque,id_categorias) values ("Espinafre","Mostarda",8.45,17,3);
+insert into tb_produtos (nome,fazenda,preco,quantidade_estoque,id_categorias) values ("Manga","Selvagem",65.67,53,4);
+insert into tb_produtos (nome,fazenda,preco,quantidade_estoque,id_categorias) values ("Caqui","Sanduíche",41.91,30,1);
+insert into tb_produtos (nome,fazenda,preco,quantidade_estoque,id_categorias) values ("Repolho","Raiz",14.94,76,2);
+insert into tb_produtos (nome,fazenda,preco,quantidade_estoque,id_categorias) values ("Groselha","Xilofone",22.84,15,4);
+insert into tb_produtos (nome,fazenda,preco,quantidade_estoque,id_categorias) values ("Amora","Barriga",66.61,34,1);
 
 SELECT * FROM tb_produtos WHERE preco > 50.00;
 
-SELECT * FROM tb_produtos WHERE preco BETWEEN 5.00 AND 60.00;
+SELECT * FROM tb_produtos WHERE preco BETWEEN 50.00 AND 100.00;
 
 SELECT * FROM tb_produtos WHERE nome LIKE "c%";
 
 SELECT * FROM tb_produtos INNER JOIN tb_categorias ON tb_categorias.id = tb_produtos.id_categorias;
 
-SELECT * FROM tb_produtos INNER JOIN tb_categorias ON tb_categorias.id = tb_produtos.id_categorias WHERE tb_categorias.categoria = "Cosméticos";
+SELECT * FROM tb_produtos INNER JOIN tb_categorias ON tb_categorias.id = tb_produtos.id_categorias WHERE tb_categorias.categoria = "Frutas";
 
 SELECT * FROM tb_produtos
